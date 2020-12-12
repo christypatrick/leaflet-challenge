bedfbc88de09f7ce6dcfb0d6b0283b380530f23e
Christy Patrick <christy06p@msn.com>
2020-12-11 23:14:55 -0600

    adding draft legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -67,30 +67,45 @@ d3.json(queryUrl, function(data) {
       }
     }).addTo(myMap);
   
+    
+    // color function to be used when creating the legend
+    function getColor(d) {
+    return d > 90 ? "#FF0000" :
+           d > 70  ? "#FF8C00" :
+           d > 50  ? "#FBB124" :
+           d > 30  ? "#FFFF00" :
+           d > 10  ? "#ADFF2F" :
+                    "#32CD32";
+    }
+    
     // an object legend
-    var legend = L.control({
-      position: "bottomright"
-    });
+    var legend = L.control({position: "bottomright"});
   
     // details for the legend
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
+    legend.onAdd = function (map) {
+
+      var div = L.DomUtil.create('div', 'info legend'),
   
-      var grades = [0, 1, 2, 3, 4, 5];
-      var colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-      ];
+      // var grades = [0, 1, 2, 3, 4, 5];
+          depthRange = [-10, 10, 30, 50, 70, 90],
+          labels = [];
+      // var colors = [
+      //   "#32CD32",
+      //   "#ADFF2F",
+      //   "#FFFF00",
+      //   "#FBB124",
+      //   "#FF8C00",
+      //   "#FF0000"
+      // ];
   
       // Looping through
-      for (var i = 0; i < grades.length; i++) {
+      for (var i = 0; i < depthRange.length; i++) {
         div.innerHTML +=
-          "<i style='background: " + colors[i] + "'></i> " +
-          grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        // "<i style='background: " + colors[i] + "'></i> " +
+        // grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      
+          '<i style="background:' + getColor(depthRange[i] + 1) + '"></i> ' +
+          depthRange[i] + (depthRange[i + 1] ? '&ndash;' + depthRange[i + 1] + '<br>' : '+');
       }
       return div;
     };
